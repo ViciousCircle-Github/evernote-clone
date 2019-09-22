56ad79a6b1f4bdfa65bbc513e4f2b4c6d819c968
ViciousCircle-Github <Stinnnkey0@gmail.com>
2019-09-23 01:38:35 +0200

    sidebar and editor added

--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { withStyles } from '@material-ui/icons/styles';
+import { withStyles } from '@material-ui/core/styles';
 import styles from './styles';
 import List from '@material-ui/core/List';
 import { Divider, Button} from '@material-ui/core';
